1410e9dbe81cd8a58ff242a6b8038f14bb592e0b
piotr921 <piotr92345@o2.pl>
2017-10-31 14:11:02 +0100

    Rebuild algorithm for min max sum.

--- src/main/java/MinMaxSum.java
+++ src/main/java/MinMaxSum.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -10,28 +8,24 @@ public class MinMaxSum {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Map<Integer, Long> numbers = new HashMap<>();
-        List<Long> sumList = new ArrayList<>(5);
+        List<Long> numbers = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            numbers.put(i, in.nextLong());
+            numbers.add(in.nextLong());
         }
 
-        for (int i = 0; i < 5; i++) {
-            final Integer currentSkip = i;
-            Optional<Long> sum = numbers.entrySet().stream()
-                    .filter(entry -> !entry.getKey().equals(currentSkip))
-                    .map(Map.Entry::getValue)
-                    .reduce(Long::sum);
-
-            sumList.add(sum.orElse(0L));
-        }
-
-        Optional<Long> max = sumList.stream()
+        Optional<Long> max = numbers.stream()
                 .max(Comparator.comparingLong(Long::longValue));
 
-        Optional<Long> min = sumList.stream()
+        numbers.remove(max.orElse(0L));
+        Optional<Long> minSum = numbers.stream().reduce(Long::sum);
+        numbers.add(max.orElse(0L));
+
+        Optional<Long> min = numbers.stream()
                 .min(Comparator.comparingLong(Long::longValue));
 
-        System.out.println(min.orElse(0L) + " " + max.orElse(0L));
+        numbers.remove(min.orElse(0L));
+        Optional<Long> maxSum = numbers.stream().reduce(Long::sum);
+
+        System.out.println(minSum.orElse(0L) + " " + maxSum.orElse(0L));
     }
 }
